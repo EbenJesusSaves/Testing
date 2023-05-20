3d8509792b29973dbe26396d8f81ce3cd93f914e
Ebenezer <agyekumebenezer2001@gmail.com>
2023-05-20 07:14:14 +0000

    started TDD

--- src/components/SampleTest.test.tsx
+++ src/components/SampleTest.test.tsx
@@ -5,7 +5,14 @@ import { SampleTest } from "./SampleTest"
 
 test('first Test', ()=>{
 render(<SampleTest/>)
-const testOne = screen.getByText('SampleTest')
+const testOne = screen.getByText(/Sampletest/i)
 expect(testOne).toBeInTheDocument()
+
+})
+
+test( 'secondTest', ()=>{
+
+render (<SampleTest name='Hellooo'/>)
+const testTwo = screen.getAllByText( /Sampletest hellooo/i)
 
 })
